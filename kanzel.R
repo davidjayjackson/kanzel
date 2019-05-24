1fd2ace7adff1451e5da6ca06a750ab7e434b70e
davidjayjackson <davidjayjackson@gmail.com>
2019-05-24 13:53:33 -0400

    Added moving average for recent observation i.e. Year >=2018. Changed basic plot.

--- kanzel.R
+++ kanzel.R
@@ -12,21 +12,19 @@ kanzel <- fread("../db/kanzel.html")
 kanzel$Ymd <- as.Date(kanzel$Ymd)
 kanzel$Year <- year(kanzel$Ymd)
 kanzel$Month <- month(kanzel$Ymd)
-kanzel<-kanzel[,.(Ymd,Year,Month,g_n,s_n,g_s,s_s,R_n,R_s,Rr,R)]
+kanzel<-kanzel[,.(Ymd,Year,Month,g_n,s_n,g_s,s_s,R_n,R_s,R)]
 
 kanzel$Short <- ma(kanzel$R,order=27)
 kanzel$Medium <- ma(kanzel$R,order=180)
 kanzel$Long <- ma(kanzel$R, order=390)
-kanzel_test <- kanzel[Ymd >=2014,]
-kanzel_train <- kanzel[Ymd <2014,]
+kanzel_test <- kanzel[Year >=2014,]
+kanzel_train <- kanzel[Year <2014,]
+kanzel_recent <- kanzel[Year >=2017,]
 
-ggplot(data=kanzel,aes(x=Ymd,y=Medium)) + geom_line()
-a <- ggplot(data=bremen,aes(x=Ymd,y=mgii)) + geom_line()
-ggplotly(a)
-#
-
+ggplot(data=kanzel_recent,aes(x=Ymd,y=R)) + geom_line() + geom_smooth(method="loess")
+ggplotly(p)
 # SQLite Stuff
 db <- dbConnect(SQLite(), dbname="kanzel.sqlite3")
-dbListTables(db)
 kanzel$Ymd <- as.character(kanzel$Ymd)
 dbWriteTable(db,"kanzel",kanzel, row.names=FALSE,overwrite=TRUE)
+dbListTables(db)
