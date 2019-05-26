add1685ba0a0798d7572f91a64ece3cc5c4b2cae
davidjayjackson <davidjayjackson@gmail.com>
2019-05-25 20:40:43 -0400

    Tweeked moving averages. upload lastest sqlite3 db.

--- kanzel.R
+++ kanzel.R
@@ -12,16 +12,18 @@ kanzel <- fread("../db/kanzel.html")
 kanzel$Ymd <- as.Date(kanzel$Ymd)
 kanzel$Year <- year(kanzel$Ymd)
 kanzel$Month <- month(kanzel$Ymd)
-kanzel<-kanzel[,.(Ymd,Year,Month,g_n,s_n,g_s,s_s,R_n,R_s,R)]
+kanzel<-kanzel[,.(Ymd,Year,g_n,s_n,g_s,s_s,R_n,R_s,R)]
 
 kanzel$Short <- ma(kanzel$R,order=27)
-kanzel$Medium <- ma(kanzel$R,order=180)
+kanzel$Medium <- ma(kanzel$R,order=60)
 kanzel$Long <- ma(kanzel$R, order=390)
 kanzel_test <- kanzel[Year >=2014,]
 kanzel_train <- kanzel[Year <2014,]
-kanzel_recent <- kanzel[Year >=2017,]
-
-ggplot(data=kanzel_recent,aes(x=Ymd,y=R)) + geom_line() + geom_smooth(method="loess")
+kanzel_recent <- kanzel[Year >=2019,]
+# Export kanzel traing data
+write.csv(kanzel_train,file="kanzel_train.csv",row.names = F)
+write.csv(kanzel_test,file="kanzel_test.csv",row.names = F)
+ggplot(data=kanzel_recent,aes(x=Ymd,y=R)) + geom_line() + geom_smooth(method="lm")
 ggplotly(p)
 # SQLite Stuff
 db <- dbConnect(SQLite(), dbname="kanzel.sqlite3")
